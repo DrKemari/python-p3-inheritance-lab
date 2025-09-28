83f7ac3222a714d0bd68554c1dac22bec3743913
Matara Kemari <drkemari@gmail.com>
2025-09-28 10:53:43 +0300

    Final

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
-
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # inherit first_name, last_name
+        self.knowledge = []  # start with an empty knowledge list
+
+    def learn(self, info):
+        self.knowledge.append(info)  # add new knowledge string
